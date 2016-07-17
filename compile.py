f33db760068df06ae11294411deef10bdd79c9af
AeonLucid <aeonlucid@outlook.com>
2016-07-17 02:43:50 +0200

    Fixed the paths and added an out argument.

--- compile.py
+++ compile.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 
-import fnmatch
-import os
-import ntpath
-import sys
-import shutil
 import argparse
+import fnmatch
+import ntpath
+import os
+import shutil
 from subprocess import call
 
 # Add this to your path
@@ -14,30 +13,35 @@ protoc_path = "protoc"
 # Specify desired language
 parser = argparse.ArgumentParser()
 parser.add_argument("-l", "--lang", help="Language to produce protoc files")
+parser.add_argument("-o", "--out_path", help="Output path for protoc files")
 args = parser.parse_args()
 lang = args.lang or "csharp"
+out_path = args.out_path or "out"
 
 # Determine where to store
 proj_root = os.path.abspath("../")
-proto_proj_path = os.path.abspath(proj_root + "/POGOLib/Pokemon/Proto")
 proto_path = os.path.abspath("pogo/")
-shutil.rmtree(proto_proj_path)
+out_path = os.path.abspath(out_path)
+
+# Clean up previous
+if os.path.exists(out_path):
+    shutil.rmtree(out_path)
 
 # Find protofiles and compile
-for root, dirnames, filenames in os.walk('pogo'):
+for root, dirnames, filenames in os.walk(proto_path):
     for filename in fnmatch.filter(filenames, '*.proto'):
         proto_file = os.path.join(root, filename)
-        relative_path = proto_file.replace("/" + ntpath.basename(proto_file), "")
-        relative_path = relative_path.replace("pogo", "")
+        relative_file_path = proto_file.replace(proto_path, "")
+        relative_path = relative_file_path.replace(ntpath.basename(proto_file), "")
 
-        destination_path = os.path.abspath(proto_proj_path + "/" + relative_path)
+        destination_path = os.path.abspath(out_path + relative_path)
 
         if not os.path.exists(destination_path):
             os.makedirs(destination_path)
 
-        print("Compiling " + proto_file + "..")
+        print("Compiling " + relative_file_path + "..")
 
-        command = """{0} --proto_path='{1}' --{2}_out='{3}' '{4}'""".format(
+        command = """{0} --proto_path="{1}" --{2}_out="{3}" "{4}\"""".format(
             protoc_path,
             proto_path,
             lang,
@@ -45,6 +49,6 @@ for root, dirnames, filenames in os.walk('pogo'):
             os.path.abspath(proto_file)
         )
 
-        call(command,shell=True)
+        call(command, shell=True)
 
 print("Done!")
