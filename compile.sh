18af6391c5c8f659fde97bf228d109d9d4f272bf
s-amine <hafoud.mohammed.amine@gmail.com>
2016-07-18 15:25:41 +0200

    Now compile.sh can compile proto files without compile.py

--- compile.sh
+++ compile.sh
@@ -1,2 +1,23 @@
 #~/bin/bash
-./compile.py 
+
+
+# 
+
+if [ ! $1 ];
+then 
+	echo "usage: $0 [lang]"
+	exit 1
+fi
+
+lang=$1
+
+directories=$(find pogo/ -type d -print0 | while read -d $'\0' file; do echo -n "-I $file "; done)
+protos=$(find pogo/ -type f -name "*.proto" -print0| sed 's/\x00/ /g')
+
+output_dir=$lang"_out/"
+
+mkdir -p $output_dir
+
+protoc $directories --"$lang"_out=$output_dir $protos
+
+echo '[+] Done'
